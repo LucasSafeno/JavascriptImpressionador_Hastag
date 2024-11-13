b12a4a09abba96f6302f28fbe70db06463a3fc5d
Lucas Safeno <lugnorio@gmail.com>
2024-11-13 21:45:49 +0000

    2. Dados, Variáveis e Operações =2. Variáveis e Constantes #F12

---
+++ 02-dados_variaveis_operacoes/02_varisveis_constantes.js
@@ -0,0 +1,13 @@
+/**
+ * ! const
+ * *    -> Constante que não pode ser alterado
+ * !let
+ * *    -> Variavel ( pode ser alterado)
+ */
+const mensagemTela = "Olá meu nome é Lucas!";
+let nome = "Lucas";
+console.log(nome)
+nome = "Lucas Tenório"
+console.log(nome);
+
+console.log(mensagemTela);
