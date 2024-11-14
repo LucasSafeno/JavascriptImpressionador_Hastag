ca85c3aa02c240a0ac1e2475c4d35f4d761bdea6
Lucas Safeno <lugnorio@gmail.com>
2024-11-14 18:22:36 +0000

    2. Dados, Variáveis e Operações = 3. Declarando e Definindo Variáveis #013

---
+++ 02-dados_variaveis_operacoes/03_declarandoVariaveis.js
@@ -0,0 +1,9 @@
+/**
+ * * Variaveis permitida com $ e _
+ * ! não pode ter espaço
+ * ! let variavel nome = 'Lucas'
+ * ! não pode começar com números
+ * * variaveis devem começar apenas com letras ou cifrão ou underline
+ * ! Não pode usar palavras reservadas
+ */
+
