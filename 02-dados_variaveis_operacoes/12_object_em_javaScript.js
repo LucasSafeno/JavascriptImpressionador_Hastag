0290e493015d8cd2fb75d37d913bc1a1d963cd94
Lucas Safeno <lugnorio@gmail.com>
2024-11-18 19:00:04 +0000

    2. Dados, Variáveis e Operações =12. Object em JavaScript #F022

---
+++ 02-dados_variaveis_operacoes/12_object_em_javaScript.js
@@ -0,0 +1,10 @@
+const documentoIdentidade = {
+  nome: "Lucas",
+  sobrenome: "Tenório",
+  cpf: "111.111.11-11",
+  rg: "111.111-11",
+  empresa: "AtualTech",
+  idade: 30,
+};
+
+console.log(documentoIdentidade.nome);
